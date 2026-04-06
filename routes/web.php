4ef121f3f4d7b65ebbc3b14baeed3e6d5a497d80
Viktor <st.viktorr@gmail.com>
2026-04-06 15:33:07 +0300

    create posts

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->file(base_path('frontend/build/main.html'));
+    return response()->file( public_path('main.html'));
 });
